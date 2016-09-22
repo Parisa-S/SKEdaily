bf021f6ef7e2b424fe8ce57e624b9e94e6909ddd
Parisa Supitayakul <parisa.s@ku.th>
2016-09-22 23:57:54 +0700

    add newsfeed

---
+++ app/src/main/java/com/example/momo/skedaily/Adapter/NewsAdapter.java
@@ -0,0 +1,40 @@
+package com.example.momo.skedaily.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.momo.skedaily.News;
+
+import java.util.List;
+
+/**
+ * Created by Momo on 22/9/2559.
+ */
+
+public class NewsAdapter extends ArrayAdapter<News>{
+    private List<News> objects;
+
+    public NewsAdapter(Context context, int resource, List<News> objects) {
+        super(context, resource, objects);
+        this.objects = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if(v==null){
+            LayoutInflater vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.news_cell, null);
+        }
+
+        TextView name = (TextView) v.findViewById(R.id.name_newsfeed);
+        TextView time = (TextView) v.findViewById(R.id.time_newsfeed);
+        TextView text = (TextView) v.findViewById(R.id.msg_newsfeed);
+
+        return super.getView(position, convertView, parent);
+    }
+}
--- app/src/main/java/com/example/momo/skedaily/MainActivity.java
+++ app/src/main/java/com/example/momo/skedaily/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.momo.skedaily;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.example.momo.skedaily.Adapter.NewsAdapter;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private List<News> news;
+    private ListView newsList;
+    private NewsAdapter newsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +59,31 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        initComponent();
+    }
+    public void initComponent(){
+        news = new ArrayList<News>();
+        addNews();
+
+        newsList = (ListView) findViewById(R.id.news_list);
+        newsAdapter = new NewsAdapter(this, R.layout.news_cell,news);
+        newsList.setAdapter(newsAdapter);
+
     }
 
+    public void addNews(){
+        News news1 = new News("Pongsakorn","20/9/2559","Algo class Cancel","/img/pic.jpg");
+        News news2 = new News("Pongsakorn","20/9/2559","Algo class Cancel","/img/pic.jpg");
+        News news3 = new News("Pongsakorn","20/9/2559","Algo class Cancel","/img/pic.jpg");
+        News news4 = new News("Pongsakorn","20/9/2559","Algo class Cancel","/img/pic.jpg");
+        news.add(news1);
+        news.add(news2);
+        news.add(news3);
+        news.add(news4);
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -89,13 +131,53 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
         } else if (id == R.id.nav_send) {
+
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.momo.skedaily/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.momo.skedaily/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
